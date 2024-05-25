78ac98ce9324b0d7911b59a67fb2f723014cb5fd
ArpanaM <muriki.arpana@gmail.com>
2024-05-25 09:22:43 +0530

    Added new class i.e Sample1

---
+++ src/main/java/Scenarios/Sample1.java
@@ -0,0 +1,10 @@
+package Scenarios;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Welcome to Git and Github...");
+	}
+
+}
